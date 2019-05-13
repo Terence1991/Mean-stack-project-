9b69b519e18d0d912027a83fa028ca5cd0124b24
terence1991 <cplterencehawes@gmail.com>
2019-05-13 12:49:46 -0400

    server set up

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log('hello')
+const http = require('http')
+
+
+ const server = http.createServer((req, res) => {
+  res.end('this is my first response')
+})
+
+server.listen(process.env.PORT || 3000)
