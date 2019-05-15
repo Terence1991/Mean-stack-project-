f415c321931adbc27a32d506098d52b4e8285f53
terence1991 <cplterencehawes@gmail.com>
2019-05-15 15:26:32 -0400

    post

--- node-04-finished/backend/app.js
+++ node-04-finished/backend/app.js
@@ -39,23 +39,13 @@ app.post("/api/posts", (req, res, next) => {
   });
 });
 
-app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts
-  });
-});
+app.get('api/posts', (req, res, next) => {
+  Post.find().then((documents)=> {
+    res.status(200).json({
+      message: 'Posts fetched succsessfully',
+      post: documents
+    })
+  })
+})
 
 module.exports = app;
